abb0ea5958281e0a4d23335f362ac4a50c483c25
Sebastian Boehm <sebastian@sometimesfood.org>
2011-05-27 14:50:48 +0200

    Use grayscale image for census transform

--- censusTransformImage.m
+++ censusTransformImage.m
@@ -1,9 +1,10 @@
 function censusTransformedImage = censusTransformImage(image)
-censusTransformedImage = zeros(size(image)-2, 'uint8');
-[height, width] = size(image);
+grayImage = rgb2gray(image);
+censusTransformedImage = zeros(size(grayImage)-2, 'uint8');
+[height, width] = size(grayImage);
 for x = 2:width-1
   for y = 2:height-1
-    censusTransformedImage(y-1, x-1) = censusTransform(image, x, y);
+    censusTransformedImage(y-1, x-1) = censusTransform(grayImage, x, y);
   end
 end
 end
