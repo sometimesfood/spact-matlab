238d4f9c71d769ffb105a8965010ea471ab07b17
Sebastian Boehm <sebastian@sometimesfood.org>
2011-05-27 14:39:11 +0200

    Let censusTransformImage return a uint8 matrix

--- censusTransformImage.m
+++ censusTransformImage.m
@@ -1,5 +1,5 @@
 function censusTransformedImage = censusTransformImage(image)
-censusTransformedImage = zeros(size(image)-2);
+censusTransformedImage = zeros(size(image)-2, 'uint8');
 [height, width] = size(image);
 for x = 2:width-1
   for y = 2:height-1
