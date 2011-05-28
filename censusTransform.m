ca02b6e3273e80fac5a969dc8a6a5a694dc9fb83
Sebastian Boehm <sebastian@sometimesfood.org>
2011-05-28 17:08:46 +0200

    Improve census transform performance (by a factor of 2)

--- censusTransform.m
+++ censusTransform.m
@@ -1,22 +1,31 @@
 function censusTransformValue = censusTransform(image, x, y)
 
-  function neighbours = neighbourPixels(image, x, y)
-    neighbours = [image(y-1, x-1)...
-                  image(y-1, x  )...
-                  image(y-1, x+1)...
-                  image(y  , x-1)...
-                  image(y  , x+1)...
-                  image(y+1, x-1)...
-                  image(y+1, x  )...
-                  image(y+1, x+1)];
-  end
-
 censusTransformValue = uint8(0);
 center = image(y, x);
-neighbours = neighbourPixels(image, x, y);
-for i = 1:length(neighbours)
-  if center >= neighbours(i)
-    censusTransformValue = bitset(censusTransformValue, i);
-  end
+
+if center >= image(y-1, x-1)
+  censusTransformValue = bitset(censusTransformValue, 1);
 end
+if center >= image(y-1, x)
+  censusTransformValue = bitset(censusTransformValue, 2);
+end
+if center >= image(y-1, x+1)
+  censusTransformValue = bitset(censusTransformValue, 3);
+end
+if center >= image(y, x-1)
+  censusTransformValue = bitset(censusTransformValue, 4);
+end
+if center >= image(y, x+1)
+  censusTransformValue = bitset(censusTransformValue, 5);
+end
+if center >= image(y+1, x-1)
+  censusTransformValue = bitset(censusTransformValue, 6);
+end
+if center >= image(y+1, x)
+  censusTransformValue = bitset(censusTransformValue, 7);
+end
+if center >= image(y+1, x+1)
+  censusTransformValue = bitset(censusTransformValue, 8);
+end
+
 end
