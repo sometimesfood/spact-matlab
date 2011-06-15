0e529b5d598cf9df9c23f0b1336250fe22757bed
Sebastian Boehm <sebastian@sometimesfood.org>
2011-06-15 13:17:29 +0200

    Swap rows and columns in centristDatabase

--- imhistDirectory.m
+++ imhistDirectory.m
@@ -1,8 +1,8 @@
 function histograms = imhistDirectory(directory)
 imageList = dir(fullfile(directory, '*.png'));
-histograms = zeros(256, length(imageList));
+histograms = zeros(length(imageList), 256);
 for i = 1:length(imageList)
   image = imread(fullfile(directory, imageList(i).name));
-  histograms(:, i) = imhist(image);
+  histograms(i, :) = imhist(image);
 end
 end
