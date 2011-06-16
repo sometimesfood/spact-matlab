014df7d148f851ec3c0e22f3c686ae3bf47b8b4a
Sebastian Boehm <sebastian@sometimesfood.org>
2011-06-16 17:47:56 +0200

    Don't try to convert grayscale images to grayscale

--- centrist.m
+++ centrist.m
@@ -1,5 +1,9 @@
 function censusTransformedImage = centrist(image)
-grayImage = rgb2gray(image);
+if ndims(image) == 2
+  grayImage = image;
+else
+  grayImage = rgb2gray(image);
+end
 censusTransformedImage = zeros(size(grayImage)-2, 'uint8');
 [height, width] = size(grayImage);
 for x = 2:width-1
