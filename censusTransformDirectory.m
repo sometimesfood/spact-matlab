5fbeee630d5fdc937a432aa614741f4b176f89ed
Sebastian Boehm <sebastian@sometimesfood.org>
2011-06-17 14:34:09 +0200

    Simplify censusTransformDirectory

--- censusTransformDirectory.m
+++ censusTransformDirectory.m
@@ -1,11 +1,8 @@
-function censusTransformDirectory(inputDir, outputDir)
+function censusTransformDirectory(inputDir)
 imageList = dir(fullfile(inputDir, '*.jpg'));
 for i = 1:length(imageList)
   filename = imageList(i).name;
   inputFile = fullfile(inputDir, filename);
-  image = imread(inputFile);
-  ctImage = censusTransformImage(image);
-  outputFile = fullfile(outputDir, strrep(filename, '.jpg', '.png'));
-  imwrite(ctImage, outputFile, 'PNG');
+  cachedCensusTransformImage(inputFile);
 end
 end
