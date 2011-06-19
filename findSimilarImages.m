d188745c7e83b75e18d608ab2dad89964273ecb6
Sebastian Boehm <sebastian@sometimesfood.org>
2011-06-19 17:39:37 +0200

    Add default options for directory and nImages in findSimilarImages

--- findSimilarImages.m
+++ findSimilarImages.m
@@ -1,6 +1,15 @@
-function filelist = findSimilarImages(image, directory)
+function filelist = findSimilarImages(image, directory, nImages)
+if ~exist('directory', 'var')
+  directory = 'images/samples';
+end
+
+if ~exist('nImages', 'var')
+  nImages = 10;
+end
+
 load(fullfile(directory, 'cache/spactDatabase.mat'));
 spactImage = spact(image, pcaLoadings, columnMeans);
 [~, indexes] = similarItems(spactImage, spactDatabase);
 filelist = { filenames{indexes} }';
+filelist = filelist(1:nImages);
 end
