d059e8ccb5b4656d4f282983345fba4e728cbce4
Florian Judex <florian.judex@ait.ac.at>
2011-07-08 15:22:45 +0200

    Improve performance for the Matlab version of censusTransformImage

--- censusTransformImage.m
+++ censusTransformImage.m
@@ -1,9 +1,19 @@
 function censusTransformedImage = censusTransformImage(image)
-censusTransformedImage = zeros(size(image)-2, 'uint8');
-[height, width] = size(image);
-for x = 2:width-1
-  for y = 2:height-1
-    censusTransformedImage(y-1, x-1) = censusTransform(image, x, y);
-  end
-end
+%y-1 x-1
+censusTransformedImage=(image(2:end-1,2:end-1)>=image(1:end-2,1:end-2));
+%y-1 x
+censusTransformedImage=censusTransformedImage+(image(2:end-1,2:end-1)>=image(1:end-2,2:end-1))*2;
+%y-1 x+1
+censusTransformedImage=censusTransformedImage+(image(2:end-1,2:end-1)>=image(1:end-2,3:end))*4;
+%y x-1
+censusTransformedImage=censusTransformedImage+(image(2:end-1,2:end-1)>=image(2:end-1,1:end-2))*8;
+%y x+1
+censusTransformedImage=censusTransformedImage+(image(2:end-1,2:end-1)>=image(2:end-1,3:end))*16;
+%y+1 x-1
+censusTransformedImage=censusTransformedImage+(image(2:end-1,2:end-1)>=image(3:end,1:end-2))*32;
+%y+1 x
+censusTransformedImage=censusTransformedImage+(image(2:end-1,2:end-1)>=image(3:end,2:end-1))*64;
+%y+1 x+1
+censusTransformedImage=censusTransformedImage+(image(2:end-1,2:end-1)>=image(3:end,3:end))*128;
+censusTransformedImage=uint8(censusTransformedImage);
 end
